f896197886385c84c2b4f9cb5c293ef718a597bd
JPKitro <jpkitro@gmail.com>
2021-08-01 19:12:25 -0300

    solve github img error

--- src/script.js
+++ src/script.js
@@ -19,21 +19,21 @@ let img2 = false
 let img3 = false
 arrowRight.addEventListener('click', function(){
     if (img1 === true) {
-        headerBg.style.backgroundImage = 'url(./../images/mobile-image-hero-2.jpg)'
+        headerBg.style.backgroundImage = 'url(images/mobile-image-hero-2.jpg)'
         img1 = false
         img2 = true
         img3 = false
         headerH1.innerHTML = 'We are available all across the globe'
         headerP.innerHTML = "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
     } else if (img2 === true) {
-        headerBg.style.backgroundImage = 'url(./../images/mobile-image-hero-3.jpg)'
+        headerBg.style.backgroundImage = 'url(images/mobile-image-hero-3.jpg)'
         img1 = false
         img2 = false
         img3 = true
         headerH1.innerHTML = 'Manufactured with the best materials'
         headerP.innerHTML = 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
     } else if (img3 === true){
-        headerBg.style.backgroundImage = 'url(./../images/mobile-image-hero-1.jpg)'
+        headerBg.style.backgroundImage = 'url(images/mobile-image-hero-1.jpg)'
         img1 = true
         img2 = false
         img3 = false
@@ -43,21 +43,21 @@ arrowRight.addEventListener('click', function(){
 })
 arrowLeft.addEventListener('click', function(){
     if (img1 === true) {
-        headerBg.style.backgroundImage = 'url(./../images/mobile-image-hero-3.jpg)'
+        headerBg.style.backgroundImage = 'url(images/mobile-image-hero-3.jpg)'
         img1 = false
         img2 = false
         img3 = true
         headerH1.innerHTML = 'Manufactured with the best materials'
         headerP.innerHTML = 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
     } else if (img3 === true) {
-        headerBg.style.backgroundImage = 'url(./../images/mobile-image-hero-2.jpg)'
+        headerBg.style.backgroundImage = 'url(images/mobile-image-hero-2.jpg)'
         img1 = false
         img2 = true
         img3 = false
         headerH1.innerHTML = 'We are available all across the globe'
         headerP.innerHTML = "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
     } else if (img2 === true){
-        headerBg.style.backgroundImage = 'url(./../images/mobile-image-hero-1.jpg)'
+        headerBg.style.backgroundImage = 'url(images/mobile-image-hero-1.jpg)'
         img1 = true
         img2 = false
         img3 = false
@@ -82,21 +82,21 @@ let img2D = false
 let img3D = false
 arrowRightD.addEventListener('click', function(){
     if (img1D === true) {
-        headerBgDesktop.style.backgroundImage = 'url(./../images/desktop-image-hero-2.jpg)'
+        headerBgDesktop.style.backgroundImage = 'url(images/desktop-image-hero-2.jpg)'
         img1D = false
         img2D = true
         img3D = false
         headerH1D.innerHTML = 'We are available all across the globe'
         headerPD.innerHTML = "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
     } else if (img2D === true) {
-        headerBgDesktop.style.backgroundImage = 'url(./../images/desktop-image-hero-3.jpg)'
+        headerBgDesktop.style.backgroundImage = 'url(images/desktop-image-hero-3.jpg)'
         img1D = false
         img2D = false
         img3D = true
         headerH1D.innerHTML = 'Manufactured with the best materials'
         headerPD.innerHTML = 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
     } else if (img3D === true){
-        headerBgDesktop.style.backgroundImage = 'url(./../images/desktop-image-hero-1.jpg)'
+        headerBgDesktop.style.backgroundImage = 'url(images/desktop-image-hero-1.jpg)'
         img1D = true
         img2D = false
         img3D = false
@@ -106,21 +106,21 @@ arrowRightD.addEventListener('click', function(){
 })
 arrowLeftD.addEventListener('click', function(){
     if (img1D === true) {
-        headerBgDesktop.style.backgroundImage = 'url(./../images/desktop-image-hero-3.jpg)'
+        headerBgDesktop.style.backgroundImage = 'url(images/desktop-image-hero-3.jpg)'
         img1D = false
         img2D = false
         img3D = true
         headerH1D.innerHTML = 'Manufactured with the best materials'
         headerPD.innerHTML = 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
     } else if (img3D === true) {
-        headerBgDesktop.style.backgroundImage = 'url(./../images/desktop-image-hero-2.jpg)'
+        headerBgDesktop.style.backgroundImage = 'url(images/desktop-image-hero-2.jpg)'
         img1D = false
         img2D = true
         img3D = false
         headerH1D.innerHTML = 'We are available all across the globe'
         headerPD.innerHTML = "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
     } else if (img2D === true){
-        headerBgDesktop.style.backgroundImage = 'url(./../images/desktop-image-hero-1.jpg)'
+        headerBgDesktop.style.backgroundImage = 'url(images/desktop-image-hero-1.jpg)'
         img1D = true
         img2D = false
         img3D = false
